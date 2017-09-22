540ca7b995fe9ce8b9c3ca661e0df606199fc03d
Adam Fink <adam@keypixel.com>
2017-09-21 20:22:00 -0400

    Add files via upload

---
+++ website.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  var x = document.getElementById('menu');
+  
+  if (x.style.display === 'block') {
+      x.style.display = 'none';
+  } else {
+      x.style.display = 'block';
+  }
+}
+
+function exitFuntion() {
+  var y = document.getElementById('menu');
+    
+  if (y.style.display === 'block') {
+      y.style.display = 'none';
+  } else {
+      y.style.display = 'block';
+  }
+
+}
+ 
